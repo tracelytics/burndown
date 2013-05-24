1784ce03cf492d3bb5a465c32fe738422559e91c
C.J. Bordeleau <bordeleau.cj@gmail.com>
2013-05-24 10:46:52 -0400

    Change auto-refresh time to 1 min.

--- app/assets/javascripts/burndown.js
+++ app/assets/javascripts/burndown.js
@@ -1,6 +1,6 @@
 $(function() {
 
-    // Set page to autorefresh every 5 min if no activity
+    // Set page to autorefresh every min if no activity
      var time = new Date().getTime();
      $(document.body).bind("mousemove keypress", function(e) {
          time = new Date().getTime();
@@ -10,10 +10,10 @@ $(function() {
          if(new Date().getTime() - time >= 60000) 
              window.location.reload(true);
          else
-             setTimeout(refresh, 5000);
+             setTimeout(refresh, 10000);
      }
 
-     setTimeout(refresh, 5000);
+     setTimeout(refresh, 10000);
 
     // Magic underscore settings to allow underscore templates to play
     // nicely with Rails ERB templates!
