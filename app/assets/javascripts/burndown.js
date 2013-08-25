698b17b3e00bcedffb627eb4f9d1069ad7761506
Brian Fleming <brianfleming@gmail.com>
2013-08-24 23:15:10 -0400

    add milestone countdown text. [#4]

--- app/assets/javascripts/burndown.js
+++ app/assets/javascripts/burndown.js
@@ -286,6 +286,11 @@ $(function() {
         getPercentComplete: function() {
             return ((this.get('closed_issues') / (this.get('open_issues') + this.get('closed_issues'))) * 100).toFixed(1);
         },
+        getMilestoneCountdown: function() {
+            var created_at = new Date(this.get('created_at'));
+            var due_at = new Date(this.get('due_on'));
+            return moment(due_at).from(created_at, true);
+        },
         getCreator: function() {
             var rval = '';
             var creator = this.get('creator');
