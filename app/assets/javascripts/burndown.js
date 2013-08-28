d18a69f2e898c867c127c793a768535b0038c9ec
Dan Riti <driti@tracelytics.com>
2013-08-28 01:17:58 +0000

    Add Label model and Labels collection. #37

--- app/assets/javascripts/burndown.js
+++ app/assets/javascripts/burndown.js
@@ -373,6 +373,39 @@ $(function() {
         }
     });
 
+    var Label = Backbone.Model.extend({
+        defaults: {
+            'name': '',
+            'color': ''
+        }
+    });
+    var Labels = Backbone.Collection.extend({
+        model: Label,
+        initialize: function() {
+            _.bindAll(this, 'isDupe', 'addLabelsFromIssues');
+        },
+        isDupe: function(name) {
+            var self = this;
+            return self.any(function(issue) {
+                return issue.get('name') === name;
+            });
+        },
+        addLabelsFromIssues: function(issues) {
+            var self = this;
+
+            // Iterate through each issue's label array.
+            _.each(issues, function(issue) {
+                _.each((issue.get('labels') || []), function(label) {
+                    // If the label has a name and is not in the collection
+                    // yet, add it!
+                    if (label.name && !self.isDupe(label.name)) {
+                        self.add(new Label(label));
+                    }
+                });
+            });
+        }
+    });
+
     // Views
     var RepoView = Backbone.View.extend({
         el: '.content',
