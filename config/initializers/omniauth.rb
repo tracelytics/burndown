9a9b75b1d64a38a6e4289de489858a8be30a2a25
Dan Riti <dmriti@gmail.com>
2014-02-10 22:11:20 +0000

    Update omniauth to support Github enterprise #47

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,13 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
+  options = {}
+  if Github.hostname?
+      options = {
+        :client_options => {
+          :site => Github.hostname,
+          :authorize_url => "#{Github.hostname}/login/oauth/authorize",
+          :token_url => "#{Github.hostname}/login/oauth/access_token",
+        }
+      }
+  end
+  provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET'], **options
 end
