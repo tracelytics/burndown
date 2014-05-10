453f9a3446e14f93c9bd88d04f22a7ab96289813
Dan Riti <dmriti@gmail.com>
2014-05-10 01:50:07 +0000

    Refactor to load all milestones #57

--- app/assets/javascripts/burndown.js
+++ app/assets/javascripts/burndown.js
@@ -70,6 +70,109 @@ $(function() {
         }
     });
 
+    var PaginatedCollection = Backbone.Collection.extend({
+        model: null,
+        comparator: null,
+        url: null,
+        parse: function(response) {
+            return response;
+        },
+        /*
+         * parse_link_header()
+         *
+         * Parse the Github Link HTTP header used for pageination
+         * http://developer.github.com/v3/#pagination
+         */
+        parseLinkHeader: function (header) {
+          if (header.length == 0) {
+              throw new Error("input must not be of zero length");
+          }
+
+          // Split parts by comma
+          var parts = header.split(',');
+          var links = {};
+          // Parse each part into a named link
+          _.each(parts, function(p) {
+              var section = p.split(';');
+              if (section.length != 2) {
+                  throw new Error("section could not be split on ';'");
+              }
+              var url = section[0].replace(/<(.*)>/, '$1').trim();
+              var name = section[1].replace(/rel="(.*)"/, '$1').trim();
+              links[name] = url;
+          });
+
+          return links;
+        },
+        parseLastPage: function(last) {
+          if (last.length == 0) {
+              throw new Error("input must not be of zero length");
+          }
+
+          var patt = /&page=(\d+)/g;
+          var result = patt.exec(last);
+
+          if (result.length != 2) {
+              throw new Error("regex pattern match failed");
+          }
+
+          return result[1];
+        },
+        getLastPage: function(header) {
+            var self = this;
+
+            // If header doesn't exist, there aren't multiple pages of results,
+            // so just return 1.
+            if (header == null) {
+                return 1;
+            }
+
+            var parsed = self.parseLinkHeader(header);
+            var last = parsed.last || '';
+            return self.parseLastPage(last);
+        },
+        all: function(progressCallback) {
+            var self = this;
+
+            var deferred = $.Deferred();
+
+            if (progressCallback) {
+                deferred.progress(progressCallback);
+            }
+
+            var currentPage = 1;
+            var lastPage = 1;
+            var success = function(issues, response, options) {
+                // Only parse the last page on the first pass.
+                if (currentPage === 1) {
+                    var header = options.xhr.getResponseHeader('Link');
+                    lastPage = self.getLastPage(header);
+                }
+                // Only continue fetching if there are pages remaining.
+                if (currentPage < lastPage) {
+                    currentPage++;
+                    deferred.notify(currentPage / lastPage);
+                    self.fetch({
+                        data: {page: currentPage},
+                        remove: false,
+                        success: success
+                    });
+                } else {
+                    console.log('end! total pages:', currentPage);
+                    deferred.resolve();
+                }
+            }
+
+            self.fetch({
+                data: {page: currentPage},
+                remove: false,
+                success: success
+            });
+
+            return deferred.promise();
+        }
+    });
+
     var Issue = Backbone.Model.extend({
         getClosedTime: function() {
             var closed = this.get('closed_at') || 0;
@@ -392,8 +495,11 @@ $(function() {
             return (this.get('state') == 'open');
         }
     });
-    var Milestones = Backbone.Collection.extend({
+    var Milestones = PaginatedCollection.extend({
         model: Milestone,
+        comparator: function(milestone) {
+            return milestone.get('created_at');
+        },
         url: function() {
             var token = session.get('token');
             var owner = session.get('owner');
@@ -511,9 +617,6 @@ $(function() {
 
             self.message = new Message();
 
-            milestones.on('sync', function() {
-                self.render();
-            });
             milestones.on('error', self.errorHandler, this);
         },
         render: function() {
@@ -551,7 +654,11 @@ $(function() {
 
             // Fetch the milestones.
             milestones.state = state;
-            milestones.fetch();
+            $.when(milestones.all())
+                .done(function(response) {
+                    console.log('all milestones fetched! ', milestones.models.length);
+                    self.render();
+                });
         },
         getInputText: function() {
             var self = this;
@@ -1122,16 +1229,9 @@ $(function() {
 
     router.on('route:milestone', function(owner, repo, id) {
         console.log('Load the milestone page!');
-        // load token
-        // load owner/repo
-        // load milestones
-        // renders repoView
-        // safe: load milestoneView!
+        // load milestoneView!
         var state = milestones.state || 'open';
-        repoView.loadRepoMilestones(owner, repo, state);
-        milestones.once('sync', function() {
-            milestoneView.loadMilestone(id);
-        });
+        milestoneView.loadMilestone(id);
     });
 
     router.on('route:repository', function(owner, repo, state) {
